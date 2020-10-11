2a7edf02f60d41e523588afaa47687b1d1d58d97
ReiKohaku <hbsjzjwj@163.com>
2020-10-11 22:05:51 +0800

    Replace BanGround Player with BanG Player

--- src/MappingPage/Pages/MetaPage/index.tsx
+++ src/MappingPage/Pages/MetaPage/index.tsx
@@ -22,8 +22,8 @@ const Links = () =>
       <Link target="_blank" rel="noopener noreferrer" href="https://bestdori.com">
         Bestdori
     </Link>&nbsp;|&nbsp;
-    <Link target="_blank" rel="noopener noreferrer" href="https://player.banground.fun">
-        Banground Player
+    <Link target="_blank" rel="noopener noreferrer" href="https://cn.bangplayer.live">
+        BanG Player v2 中国版
     </Link>&nbsp;|&nbsp;
     <Link target="_blank" rel="noopener noreferrer" href="https://github.com/K024/bangbangboom-editor">
         About bangbangboom editor & translations
